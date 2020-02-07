e37c0c884e3ff59c110a5b1a5c5d556a0cff7d9e
Leandro de Oliveira <leandro-smi@hotmail.com>
2020-02-07 03:13:39 -0300

    Update jogo_da_velha.py

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,6 +1,7 @@
 from random import choice
 import sys
 import time
+import json
 
 print('''
 ##############################################
@@ -17,10 +18,7 @@ print('''
 ##############################################
 ''')
 
-print(''' 
-CARA:  VOCÊ É O JOGADOR "X"
-COROA: VOCÊ É O JOGADOR "O"
-BOA SORTE!''')
+print(''' CARA:  VOCÊ É O JOGADOR "X"\nCOROA: VOCÊ É O JOGADOR "O"\nBOA SORTE!''')
 
 print("JOGANDO A MOEDA!")
 for i in range(5,0,-1):
@@ -28,23 +26,15 @@ for i in range(5,0,-1):
     sys.stdout.flush()
     time.sleep(1)
 
-d = ['CARA', 'COROA']
+d = [('CARA', 'X'), ('COROA', 'O')]
 e = choice(d)
+print(f'\nO resultado foi "{e[0]}", você é o jogador "{e[1]}"!')
 
-if e == 'CARA':
-        f = 'X'
-else:
-        f = 'O'
-        
-print(f'\nO resultado foi "{e}", você é o jogador "{f}"!')
-
-j = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+j = [' ']*9
 
 print('''
-###############################################
-# A tabela do jogo é representada por campos, #
-# escolha o campo que pretende preencher.     #
-###############################################
+###############################################\n# A tabela do jogo é representada por campos, #
+# escolha o campo que pretende preencher.     #\n###############################################
       ''')
 
 m = """###############################################            
@@ -59,88 +49,64 @@ ar = [line.strip() for line in m.split('\n')]
 for line in ar:
         print(line.strip())
 
-# As variaveis a seguir é de critério de comparação mais a frente
-
+# As variaveis a seguir são de critério de comparação mais a frente
 c = 1
-a = '11'
-b = '12'
+a = '11' # maior que o número de jogadas possiveis no caso é 9 (dimensão do tabuleiro)
+b = '12' # diferente e maior que 'a'
+
+def jogada(jog):
+	pos = input(f'Jogador "{jog}", onde pretende fazer a jogada? ')
+	q = 1
+	while q == 1:
+		if pos.isnumeric()and int(pos) <= 8 and j[int(pos)] == ' ':	
+			pos = int(pos)
+			break
+		else:
+			if pos.isnumeric() and int(pos) <= 8 and j[int(pos)] != ' ':
+				pos = input(f'Jogador "{jog}" esse campo já foi preenchido, por favor escolha outro: ')
+			else:
+				pos = input(f'Jogador "{jog}", digite um caractere numérico entre 0 e 8: ')
+	j[pos] = jog
+	
+
 
 while c <= 10:
 
-        if ((j.count('X') == 4 and j.count('O') == 5) or\
-            (j.count('X') == 5 and j.count('O') == 4)):
-                print('EMPATE!')
-                break
+	if ((j.count('X') == 4 and j.count('O') == 5) or\
+		(j.count('X') == 5 and j.count('O') == 4)):
+			print('EMPATE!')
+			break 
+	
+	if c % 2 == 1:
+		jogador = 'X'
+	else:
+		jogador = 'O'
 
-        if ((j[0] == j[1] == j[2] == 'X') or\
-            (j[2] == j[5] == j[8] == 'X') or\
-            (j[6] == j[7] == j[8] == 'X') or\
-            (j[0] == j[3] == j[6] == 'X') or\
-            (j[0] == j[4] == j[8] == 'X') or\
-            (j[2] == j[4] == j[6] == 'X') or\
-            (j[1] == j[4] == j[7] == 'X') or\
-            (j[3] == j[4] == j[5] == 'X')):
-                print('Jogador "X" é o vencedor!')
-                break
+	jogada(jogador)
 
-        if ((j[0] == j[1]  == j[2] == 'O') or\
-            (j[2] == j[5]  == j[8] == 'O') or\
-            (j[6] == j[7]  == j[8] == 'O') or\
-            (j[0] == j[3]  == j[6] == 'O') or\
-            (j[2] == j[4]  == j[6] == 'O') or\
-            (j[1] == j[4]  == j[7] == 'O') or\
-            (j[3] == j[4]  == j[5] == 'O') or\
-            (j[0] == j[4]  == j[8] == 'O')):
-                print('Jogador "O" é o vencedor!')
-                break
+	print(jogador)
 
-        if c % 2 == 1:
+	m = """###############################################         
+			#             Campos      Posições            #
+			#           0 | 1 | 2    {} | {} | {}            #
+			#          ---+---+---  ---+---+---           #    
+			#           3 | 4 | 5    {} | {} | {}            #
+			#          ---+---+---  ---+---+---           # 
+			#           6 | 7 | 8    {} | {} | {}            #
+			###############################################""".format(j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8])
+	arr = [line.strip() for line in m.split('\n')]
+	for line in arr:
+		print(line.strip())
 
-                a = input('Jogador "X", onde pretende fazer a jogada? ')
-                q = 1
+	if ((j[0] == j[1] == j[2] == jogador) or\
+		(j[2] == j[5] == j[8] == jogador) or\
+		(j[6] == j[7] == j[8] == jogador) or\
+		(j[0] == j[3] == j[6] == jogador) or\
+		(j[0] == j[4] == j[8] == jogador) or\
+		(j[2] == j[4] == j[6] == jogador) or\
+		(j[1] == j[4] == j[7] == jogador) or\
+		(j[3] == j[4] == j[5] == jogador)):
+		print(f'Jogador "{jogador}" é o vencedor!')
+		break
 
-                while q == 1:
-
-                        if a.isnumeric()and int(a) <= 8 and j[int(a)] == ' ':	
-                                a = int(a)
-                                break
-                        else:
-                                if a.isnumeric() and int(a) <= 8 and j[int(a)] != ' ':
-                                        a = input('Jogador "X" esse campo já foi preenchido, por favor escolha outro: ')
-                                else:
-                                        a = input('Jogador "X", digite um caractere numérico entre 0 e 8: ')
-
-                j[a] = 'X'
-
-        else:
-
-                b = input('Jogador "O", onde pretende fazer a jogada? ')
-                w = 1
-
-                while w == 1:
-
-                        if b.isnumeric() and int(b) <= 8 and j[int(b)] == ' ':	
-                                b = int(b)
-                                break
-                        else:
-                                if b.isnumeric() and int(b) <= 8 and j[int(b)] != ' ':
-                                        b = input('Jogador "O" esse campo já foi preenchido, por favor escolha outro: ')
-                                else:
-                                        b = input('Jogador "O", digite um caractere numérico entre 0 e 8: ')
-
-                j[b] = 'O'
-
-        m = """###############################################         
-               #             Campos      Posições            #
-               #           0 | 1 | 2    {} | {} | {}            #
-               #          ---+---+---  ---+---+---           #    
-               #           3 | 4 | 5    {} | {} | {}            #
-               #          ---+---+---  ---+---+---           # 
-               #           6 | 7 | 8    {} | {} | {}            #
-               ###############################################""".format(j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8])
-        
-        arr = [line.strip() for line in m.split('\n')]
-        for line in arr:
-                print(line.strip())
-
-        c += 1
+	c = c + 1
