5901d516f88da4eed020a4205c2efd803a687c0d
Leandro de Oliveira <leandro-smi@hotmail.com>
2020-02-09 03:33:49 -0300

    Update jogo_da_velha.py

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,82 +1,90 @@
 from random import choice
 import sys, time, json
 
-
-print('''##############################################\n#    #     #  ###### #      #    #   ##      #
-#    #     #  #      #      #    #  #  #     #\n#     #   #   ####   #      ###### ######    #
-#      # #    #      #      #    # #    #    #\n#       #     ###### ###### #    # #    #    #
-#                                            #\n#                 VERSÃO 1.0c                #
-#          BY: Leandro de Oliveira           #\n#           Developed in: PyCharm            # 
-#                 PYTHON 3.6                 #\n##############################################''')
-print('''CARA: VOCÊ É O JOGADOR "X"\nCOROA: VOCÊ É O JOGADOR "O"\nBOA SORTE!''')
+print(
+    '''CARA: VOCÊ É O JOGADOR "X"\nCOROA: VOCÊ É O JOGADOR "O"\nBOA SORTE!\n'''
+)
 print("JOGANDO A MOEDA!")
-for i in range(5,0,-1):
-    sys.stdout.write(str('.')+' ')
+for i in range(5, 0, -1):
+    sys.stdout.write(str('.') + ' ')
     sys.stdout.flush()
     time.sleep(1)
 d = [('CARA', 'X'), ('COROA', 'O')]
 e = choice(d)
-print(f'\nO resultado foi "{e[0]}", você é o jogador "{e[1]}"!')
+print(f'\n\nO resultado foi "{e[0]}", você é o jogador "{e[1]}"!\n')
 
-j = [' ']*9
+T = [' '] * 9 # T de tabuleiro
 
-print('''###############################################\n# A tabela do jogo é representada por campos, #
-# escolha o campo que pretende preencher.     #\n###############################################''')
+print(
+    '''###############################################\n# A tabela do jogo é representada por campos, #
+# escolha o campo que pretende preencher.     #\n###############################################'''
+)
+
+
 def tabela():
-	m = """###############################################            
+    m = """\n###############################################            
 		#             Campos      Posições            #
 		#           0 | 1 | 2    {} | {} | {}            #
 		#          ---+---+---  ---+---+---           #      
 		#           3 | 4 | 5    {} | {} | {}            #
 		#          ---+---+---  ---+---+---           #  
 		#           6 | 7 | 8    {} | {} | {}            #
-		###############################################""".format(j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8])
-	ar = [line.strip() for line in m.split('\n')]
-	for line in ar:
-			print(line.strip())
-tabela()			
+		###############################################"""\
+     .format(T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8])
+    ar = [line.strip() for line in m.split('\n')]
+    for line in ar:
+        print(line.strip())
 
-def jogada(jog):
-	pos = input(f'Jogador "{jog}", onde pretende fazer a jogada? ')	
-	while 1:
-		if pos.isnumeric()and int(pos) <= 8 and j[int(pos)] == ' ':	
-			pos = int(pos)
-			break
-		else:
-			if pos.isnumeric() and int(pos) <= 8 and j[int(pos)] != ' ':
-				pos = input(f'Jogador "{jog}" esse campo já foi preenchido, por favor escolha outro: ')
-			else:
-				pos = input(f'Jogador "{jog}", digite um caractere numérico entre 0 e 8: ')
-	j[pos] = jog
-	
-# As variaveis a seguir são de critério de comparação mais a frente
+
+tabela()
+
+
+def jogada(jogador):
+    pos = input(f'Jogador "{jogador}", onde pretende fazer a jogada? ')
+    while 1:
+        if pos.isnumeric() and int(pos) <= 8 and T[int(pos)] == ' ':
+            pos = int(pos)
+            break
+        else:
+            if pos.isnumeric() and int(pos) <= 8 and T[int(pos)] != ' ':
+                pos = input(
+                    f'Jogador "{jogador}" esse campo já foi preenchido, por favor escolha outro: '
+                )
+            else:
+                pos = input(
+                    f'Jogador "{jogador}", digite um caractere numérico entre 0 e 8: '
+                )
+    T[pos] = jogador
+
+
+def situacao(t, jogador): # v de vetor
+    if ((t[0] == t[1] == t[2] == jogador) or\
+		(t[2] == t[5] == t[8] == jogador) or\
+		(t[6] == t[7] == t[8] == jogador) or\
+		(t[0] == t[3] == t[6] == jogador) or\
+		(t[0] == t[4] == t[8] == jogador) or\
+		(t[2] == t[4] == t[6] == jogador) or\
+		(t[1] == t[4] == t[7] == jogador) or\
+		(t[3] == t[4] == t[5] == jogador)):
+        print(f'Jogador "{jogador}" é o vencedor!')
+        return 'temos um vencedor'
+
+
 c = 1
-a = '11' # maior que o número de jogadas possiveis no caso é 9 (dimensão do tabuleiro)
-b = '12' # diferente e maior que 'a'
+
 while c <= 10:
 
-	if c % 2 == 1:
-		jogador = 'X'
-	else:
-		jogador = 'O'
+    XO = 'X' if c % 2 == 1 else 'O'
 
-	jogada(jogador)
+    jogada(XO)
 
-	tabela()
+    tabela()
 
-	if ((j[0] == j[1] == j[2] == jogador) or\
-		(j[2] == j[5] == j[8] == jogador) or\
-		(j[6] == j[7] == j[8] == jogador) or\
-		(j[0] == j[3] == j[6] == jogador) or\
-		(j[0] == j[4] == j[8] == jogador) or\
-		(j[2] == j[4] == j[6] == jogador) or\
-		(j[1] == j[4] == j[7] == jogador) or\
-		(j[3] == j[4] == j[5] == jogador)):
-		print(f'Jogador "{jogador}" é o vencedor!')
-		break
+    if situacao(T, XO) == 'temos um vencedor':
+        break
 
-	if c == 9:
-		print("EMPATE!")
-		break
+    if c == 9:
+        print("EMPATE!")
+        break
 
-	c = c + 1
+    c = c + 1
